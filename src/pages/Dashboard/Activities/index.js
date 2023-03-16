36b719bd23d66a6a6fd4c879ab252a075cef730a
manuelamp <manuella.m.p182@gmail.com>
2023-03-16 11:13:55 -0300

    feat: error pages in activities

--- src/pages/Dashboard/Activities/index.js
+++ src/pages/Dashboard/Activities/index.js
@@ -1,3 +1,11 @@
-export default function Activities() {
-  return 'Atividades: Em breve!';
+import { Typography } from '@material-ui/core';
+import Activities from '../../../components/Activities';
+
+export default function ChooseActivites() {
+  return (
+    <>
+      <Typography variant="h4">Escolha de Atividades</Typography>
+      <Activities />
+    </>
+  );
 }
